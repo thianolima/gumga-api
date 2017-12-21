51544a912f92cd84988cbc223cc1776b197441c6
thiano pereira lima <thianolima@hotmail.com>
2017-12-21 10:25:29 -0200

    cooreção das variaveis que ainda estavam em portugues e retirada de alguns modules que eu acabei nao utilizando no projeto

--- src/main/resources/static/js/controller/IndexController.js
+++ src/main/resources/static/js/controller/IndexController.js
@@ -1,44 +1,49 @@
 app.controller('IndexController', function($scope, $http){
-	$scope.pwd = "";
 	$scope.score = "0%";
 	$scope.complexity = "MUITO FRACO";
 	
 	
 	$scope.validate = function(){
 		
-		$http({
-			method: "POST",
-			url: "/passwordmeter/" + $scope.senha,
-			data: $scope.senha
+		if($scope.pwd == ""){
+			$scope.score = "0%";
+			$scope.complexity = "MUITO FRACO";
 		
-		}).then(function sucess(response){
-			$scope.score = response.data.score + "%";			
+		}else{
+		
+			$http({
+				method: "POST",
+				url: "/passwordmeter/" + $scope.pwd
 			
-			if(response.data.complexity == 'VERY_WEAK'){
-				$scope.complexity = 'MUITO FRACO';
-			} 
-			
-			else if(response.data.complexity == 'WEAK'){
-				$scope.complexity = 'FRACO';
-			} 
-			
-			else if(response.data.complexity == 'GOOD'){
-				$scope.complexity = 'BOM';
-			} 
-			
-			else if(response.data.complexity == 'STRONG'){
-				$scope.complexity = 'FORTE';
-			} 
-			
-			else if(response.data.complexity == 'VERY_STRONG'){
-				$scope.complexity = 'MUITO FORTE';
-			}
-			
-			console.log(response.data);
-					
-		}, function error(response){
-			
-		});
+			}).then(function sucess(response){
+				$scope.score = response.data.score + "%";			
+				
+				if(response.data.complexity == 'VERY_WEAK'){
+					$scope.complexity = 'MUITO FRACO';
+				} 
+				
+				else if(response.data.complexity == 'WEAK'){
+					$scope.complexity = 'FRACO';
+				} 
+				
+				else if(response.data.complexity == 'GOOD'){
+					$scope.complexity = 'BOM';
+				} 
+				
+				else if(response.data.complexity == 'STRONG'){
+					$scope.complexity = 'FORTE';
+				} 
+				
+				else if(response.data.complexity == 'VERY_STRONG'){
+					$scope.complexity = 'MUITO FORTE';
+				}
+							
+				console.log(response.data);
+						
+			}, function error(response){
+				
+			});
+		}
 	}
 	
 });
